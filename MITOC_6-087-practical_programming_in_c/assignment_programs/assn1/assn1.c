a635a429a8fcd8658836e2707221bafe07f9d35c
fernando hernandez <fhernandezx3fh@gmail.com>
2017-07-18 12:40:52 -0400

    modified assn1

--- MITOC_6-087-practical_programming_in_c/assignment_programs/assn1/assn1.c
+++ MITOC_6-087-practical_programming_in_c/assignment_programs/assn1/assn1.c
@@ -1,4 +1,7 @@
-/* gcc -g -O0 -Wall hello.c -o hello.o */
+/* gcc -g -O0 -Wall hello.c -o hello.o 
+ * gdb <object/executable>
+ * run
+ */
 #include <stdio.h>
 
 int main(void) // main entry point
@@ -38,7 +41,7 @@ int main(void) // main entry point
   printf("10.0+2.0/((3.0−2.0)∗2.0) = %f\n", ans);
 
   
-  /* Problem 1.2 ()
+  /* Problem 1.2 (Macros)
    * double ans2 = 18.0/squared(2+1);
    */
   #define squared(x)  x*x
@@ -48,8 +51,40 @@ int main(void) // main entry point
   double ans2 = 18.0/squared(2+1);
   printf("18.0/squared(2+1) = %f\n", ans2);
 
+  /* Problem 1.3 (Printing)
+   */
   const char msg[] = "hello, 6.087 students";
   puts(msg);
+
+
+  /* Problem 1.4 (Code Arrangement)
+   * Re-order as needed
+   * 1. return 0;
+   * 2. const char msg[] = MSG1;
+   * 3. }
+   * 4. #define MSG1 "All your base are belong to us!"
+   * 5. int main(void) {
+   * 6. #include <stdio.h>
+   * 7. puts(msg);
+   *
+   * 1. #include <stdio.h>
+   * 2. #define MSG1 "All your base are belong to us!"
+   * 3. int main(void) {
+   * 4. const char msg[] = MSG1;
+   * 5. puts(msg);
+   * 6. return 0;
+   * 7. }
+   */
+
+  /* Problem 1.3 (Printing)
+   * (a) #include <stdio.h>; Remove ;
+   * (b) int function(void arg1) Change void to int
+   *      {
+   *        return arg1-1;
+   *      }
+   * (c) #define MESSAGE = "Happy new year!" Remove =
+   * puts(MESSAGE);
+   */
 
   return 0; // exit (0 => success)
 }
