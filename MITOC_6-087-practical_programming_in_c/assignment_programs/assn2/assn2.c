957bf313c2c63a34989160ca4e19bd6b65e66322
fernando hernandez <fhernandezx3fh@gmail.com>
2017-07-25 16:36:17 -0400

    adding example problems in assn2

--- MITOC_6-087-practical_programming_in_c/assignment_programs/assn2/assn2.c
+++ MITOC_6-087-practical_programming_in_c/assignment_programs/assn2/assn2.c
@@ -5,6 +5,14 @@
 
 int main(void) // main entry point
 {
+  /* Problem 2.1
+   * Determine the size, min, and max value of char, unsigned char
+   * short, int, unsigned int, unsigned long, and float data types.
+   * Indicate whether machine is 32 or 64 bit
+   *
+   * 64 bit machine
+   */
+
   printf( "SizeOf(char) = %ld\n"
           "CHAR_MIN = %d\n"
           "CHAR_MAX = %d\n\n",
@@ -47,6 +55,41 @@ int main(void) // main entry point
           sizeof(float),
           FLT_MIN,
           FLT_MAX);
+
+  /* Problem 2.2 (Character Literals)
+   * Write logical expressions that tests whether a given character
+   * variable, c, is:
+   * lowercase letter
+   * upper case letter
+   * digit
+   * white space (includes space, tab, newline)
+   */
+
+  char c = 'a';
+  int truth = 0;
+  truth = ('a' >=  c && c <= 'z');
+  printf("%d\n", truth);
+  
+  truth = ('A' >=  c && c <= 'Z');
+  printf("%d\n", truth);
+
+  truth = ('0' >=  c && c <= '9');
+  printf("%d\n", truth);
+
+  truth = (c==' ' || c=='\t' || c=='\n');
+  printf("%d\n", truth);
+
+  /* Problem 2.3 (Bitwise Operations)
+   * Consider int val=0xCAFE;
+   * 1100 1010 1111 1110
+   * write bitwise operations that do the following:
+   * test if at least 3 of the last 4 LSB are on
+   * reverse the byte order (ie 0xFECA)
+   * rotate fourbits (ie ECAF)
+  */
+  int val = 0xCAFE;
+  int lastByte = val & 0x7;
+  printf("%d\n", lastByte);
 
   return 0; // exit (0 => success)
 }
