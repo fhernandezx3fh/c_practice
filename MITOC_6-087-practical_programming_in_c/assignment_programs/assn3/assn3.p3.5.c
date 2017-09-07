0ae90678eb92ba20e0ed464cc92d17a902f8154d
fernando hernandez <fhernandezx3fh@gmail.com>
2017-09-07 15:55:05 -0400

    modified assn3.p3.5.c

--- MITOC_6-087-practical_programming_in_c/assignment_programs/assn3/assn3.p3.5.c
+++ MITOC_6-087-practical_programming_in_c/assignment_programs/assn3/assn3.p3.5.c
@@ -6,5 +6,33 @@
 #define NFIELDS 9
 int main()
 {
+  FILE*   fp=NULL;
+  char*   fileName="stateoutflow0708.txt";
+  char    currentLine[STRSIZE];
+  char    field1[STRSIZE];
+  char    field2[STRSIZE];
+  char    field3[STRSIZE];
+  char    field4[STRSIZE];
+  char    field5[STRSIZE];
+  char    field6[STRSIZE];
+  char    field7[STRSIZE];
+  char    field8[STRSIZE];
+  char    field9[STRSIZE];
+
+  fp = fopen(fileName, "r");
+  
+  while(fscanf(fp,  "%s %s %s %s %s %s %s %s %s", \
+                    field1, field2, field3,       \
+                    field4, field5, field6,       \
+                    field7, field8, field9)==9)
+  {
+    printf(         "%s %s %s %s %s %s %s %s %s\n",
+                    field1, field2, field3,       \
+                    field4, field5, field6,       \
+                    field7, field8, field9);
+  }
+  //printf("%s %s %s %s %s %s %s %s %s\n", field1, field2, field3,field4, field5, field6,field7, field8, field9);
+
+  fclose(fp);
   return 0;
 }
